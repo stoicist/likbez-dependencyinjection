3c8608b0eaa54b2c9df5307c154f122952b73985
Arthur Igberdin <artur.igberdin@bcs-ideas.com>
2018-11-29 13:02:38 +0300

    stub for container injection

--- likbez-dependencyinjection/ViewController.swift
+++ likbez-dependencyinjection/ViewController.swift
@@ -9,16 +9,14 @@ class ViewController: UIViewController {
 
     /*
         Контейнер зависимостей - фреймворки которые помогают управлять зависимостями
+        Строго разделенный код - со слабыми связями между объектами
     */
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         propertyInjection()
-        
         initializerInjection() //with Dependency Injection
-        
         methodInjection()
         
         containerInjection()
@@ -26,4 +24,9 @@ class ViewController: UIViewController {
 }
 
 //TODO: - Реализовать контейнер зависимостей
-func containerInjection() { }
+func containerInjection() {
+    /*
+    let factory = Factory() //внутри себя хранит сервисы
+    let viewController = ViewController(factory: factory)
+    */
+}
