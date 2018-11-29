1ba0f346e2b78ff1d707c4ecd06ce45eb10b52d6
Arthur Igberdin <artur.igberdin@bcs-ideas.com>
2018-11-29 11:11:00 +0300

    Initial Commit

---
+++ likbez-dependencyinjection/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  likbez-dependencyinjection
+//
+//  Created by Arthur on 29/11/2018.
+//  Copyright © 2018 Arthur Ber. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
